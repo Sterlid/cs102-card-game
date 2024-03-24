61284090568cd56eb65dad88f45526dc568dc7d6
Sterlid <158247812+Sterlid@users.noreply.github.com>
2024-03-24 20:16:25 +0800

    Update Blackjack.java
    uncommented playerHandSum and dealerHandSum

--- src/utility/Blackjack.java
+++ src/utility/Blackjack.java
@@ -33,8 +33,8 @@ public class Blackjack {
     String result;
 
     // Hand sum
-    // String playerHandSum;
-    // String dealerHandSum;
+    String playerHandSum;
+    String dealerHandSum;
     String playerMoney;
     // UI Buttons
     JPanel buttonPanel = new JPanel();
