18395c36f1129dc88c14c035f63027595bfd0df5
Sterlid <158247812+Sterlid@users.noreply.github.com>
2024-03-24 13:26:09 +0800

    Add files via upload

---
+++ src/entities/Player.java
@@ -0,0 +1,70 @@
+package entities;
+import java.util.ArrayList;
+
+public class Player {
+    ArrayList<Card> hand;
+    boolean banBan;
+    boolean banLuck;
+    int sum;
+    int aceCount;
+    int pictureCount;
+    int money;
+
+    public Player(){
+        hand = new ArrayList<Card>();
+        banBan = false;
+        banLuck = false;
+        sum = 0;
+        aceCount =0;
+        pictureCount = 0;
+        money = 5000;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public boolean isBanBan() {
+        return banBan;
+    }
+
+    public boolean isBanLuck() {
+        return banLuck;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public int getAceCount() {
+        return aceCount;
+    }
+
+    public int getPictureCount() {
+        return pictureCount;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    public void setAceCount(int aceCount) {
+        this.aceCount = aceCount;
+    }
+
+    public void setPictureCount(int pictureCount) {
+        this.pictureCount = pictureCount;
+    }
+
+    public void setFreshHand(){
+        this.hand = new ArrayList<Card>();
+    }
+  
+    public int getMoney(){
+        return this.money;
+    }
+
+    public void setMoney(int money){
+        this.money = money;
+    }
+}
