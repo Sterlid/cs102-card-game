9460f65b3b4e15c1c986d98d7eac712d7a3c048b
Sterlid <158247812+Sterlid@users.noreply.github.com>
2024-03-27 15:49:16 +0800

    Update DeckBuilder.java
    removed shuffle, placed it in build deck

--- src/utility/DeckBuilder.java
+++ src/utility/DeckBuilder.java
@@ -3,14 +3,13 @@ import java.util.*;
 
 import entities.*;
 
-// Builds a new deck
 public class DeckBuilder {
     public static ArrayList<Card> buildDeck() {
         ArrayList<Card> deck = new ArrayList<>();
         String[] values = { "a", "2", "3", "4", "5", "6", "7", "8", "9", "t", "j", "q", "k" };
         String[] types = { "c", "d", "h", "s" };
 
-         for (String value : values) {
+        for (String value : values) {
             for (String type : types) {
                 deck.add(new Card(value, type));
             }
@@ -19,16 +18,8 @@ public class DeckBuilder {
         System.out.println("BUILD DECK:");
         System.out.println(deck);
 
-        return shuffle(deck);
-    }
-    
-    // Swap around 2 cards at a time randomly to shuffle the deck
-    public static ArrayList<Card> shuffle(ArrayList<Card> deck) {
         Collections.shuffle(deck);
-
-        System.out.println("AFTER SHUFFLE");
-        System.out.println(deck);
-
         return deck;
     }
+
 }
